250583754fcc43dc1c4c856f40221556127d9850
nivoc <kuehnle@online.de>
2013-04-17 18:02:57 +0200

    inital version

--- Degrees/main.c
+++ Degrees/main.c
@@ -8,11 +8,21 @@
 
 #include <stdio.h>
 
+float fahrenheitFromCelsius(float cel)
+{
+    float fahr = cel * 1.8 + 32.0;
+    printf("%f Celsius is %f Fahrenheit\n", cel, fahr);
+    return fahr;
+}
+
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    float freezeInC = 0;
+    float freezeInF = fahrenheitFromCelsius(freezeInC);
+    
+    printf("Water freezes at %f degrees Fahrenheit\n", freezeInF);
+    
     return 0;
 }
 
